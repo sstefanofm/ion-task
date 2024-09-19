688f70e5886c8a27c45ec8e00cdb8b4f6466876c
stf <sstefanofm@gmail.com>
2024-09-19 16:35:21 -0300

    add ion-fab-button bottom-end with icon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList } from '@ionic/angular/standalone';
+import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, IonFab, IonIcon, IonFabButton } from '@ionic/angular/standalone';
 import { MessageComponent } from '../message/message.component';
 
 import { DataService, Message } from '../services/data.service';
+import { addIcons } from 'ionicons';
+import { chevronUpOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent, IonFab, IonIcon, IonFabButton],
 })
 export class HomePage {
   private data = inject(DataService);
-  constructor() {}
+  constructor() {
+    addIcons({ chevronUpOutline })
+  }
 
   refresh(ev: any) {
     setTimeout(() => {
